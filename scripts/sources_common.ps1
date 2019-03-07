58f44de3fa88048ba00f29355e99bd85c0d94c8b
dandrejvv <dandrejvv@gmail.com>
2019-03-07 15:45:12 +0200

    BITS Transfer is not stable at all. So I found some code to have my own downloader in Powershell. Also cleaned up the scripts files some more.

--- scripts/sources_common.ps1
+++ scripts/sources_common.ps1
@@ -10,9 +10,27 @@ $ElasticSearchOssDownloadUri = "https://artifacts.elastic.co/downloads/elasticse
 $LogstashOssDownloadUri = "https://artifacts.elastic.co/downloads/logstash/${LogstashOssZipFile}"
 $KibanaOssDownloadUri = "https://artifacts.elastic.co/downloads/kibana/${KibanaOssZipfile}"
 
+function Create-Folder-If-Missing ($relativePath) {
+    if ((Test-Path -Path $relativePath) -eq $false) {
+        New-Item -ItemType directory -Force $relativePath > $null
+    }
+}
+
+function Download-If-Missing ($expectedZipLocation, $downloadUri) {
+    if ((Test-Path -Path $expectedZipLocation) -eq $false) {
+		DownloadFile $downloadUri $expectedZipLocation
+    }
+}
+
 function Copy-App ($appFolderName, $sourcePath, $destinationPath) {
-    if((Test-Path -Path ($destinationPath + $appFolderName) -Exclude *.zip) -eq $false) {
+    if ((Test-Path -Path ($destinationPath + $appFolderName) -Exclude *.zip) -eq $false) {
         Copy-Item -Recurse -Path ($sourcePath + "\\" + $appFolderName) -Destination ($destinationPath + "jdk-" + $appFolderName) -Exclude *.zip
+    }
+}
+
+function Copy-File-If-Missing ($sourceFilePath, $destFilePath) {
+    if ((Test-Path -Path $destFilePath) -eq $false) {
+        Copy-Item -Path $sourceFilePath -Destination $destFilePath -Force
     }
 }
 
@@ -30,4 +48,50 @@ function Get-JDK-App ($sourcesFolder) {
     
     $jdkFolder = $jdkFolder[0].Name
     return $jdkFolder
+}
+
+function DownloadFile {
+	# Courtesy of Jason Niver / https://blogs.msdn.microsoft.com/jasonn/2008/06/13/downloading-files-from-the-internet-in-powershell-with-progress/
+	param([string]$url, [string]$targetFile)
+
+	"Downloading $url"
+	$uri = New-Object "System.Uri" "$url"
+	$request = [System.Net.HttpWebRequest]::Create($uri)
+	$request.ServicePoint.Expect100Continue = $false;
+	$request.ProtocolVersion = [System.Net.HttpVersion]::Version11;
+	$request.set_Timeout(15000) # 15 second timeout
+	$response = $request.GetResponse()
+	Try {
+		$totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
+		
+		$responseStream = $response.GetResponseStream()
+		Try {
+			$targetStream = [System.IO.FileStream]::new($targetFile, [System.IO.FileMode]::Create)
+			Try {
+				$buffer = new-object byte[] 10KB
+				$count = $responseStream.Read($buffer,0,$buffer.length)
+				$downloadedBytes = $count
+				while ($count -gt 0)
+				{ 
+				  $downloaded = [System.Math]::Floor($downloadedBytes/1024)
+				  Write-Progress -Activity "Downloading $targetFile ..." -Status "$downloaded K / $totalLength K" -PercentComplete ($downloaded / $totalLength * 100)
+				  $targetStream.Write($buffer, 0, $count)
+				  $count = $responseStream.Read($buffer,0,$buffer.length)
+				  $downloadedBytes = $downloadedBytes + $count
+				} 
+				"`nFinished Download"
+				$targetStream.Flush()
+				$targetStream.Close()
+			}
+			Finally {
+				$targetStream.Dispose()
+			}
+		}
+		Finally {
+			$responseStream.Dispose()
+		}
+	}
+	Finally {
+		$response.Dispose()
+	}
 }
--- setup_sources.ps1
+++ setup_sources.ps1
@@ -1,36 +1,29 @@
 . "./scripts/sources_common.ps1"
 
-Import-Module BitsTransfer
+Create-Folder-If-Missing "elasticsearch\\sources"
+Create-Folder-If-Missing "logstash\\sources"
+Create-Folder-If-Missing "kibana\\sources"
 
-function DownloadIfMissing ($expectedZipLocation, $downloadUri) {
-    if((Test-Path -Path $expectedZipLocation) -eq $false) {
-        Start-BitsTransfer -Source $downloadUri -Destination $expectedZipLocation
-    }
-}
-
-New-Item -ItemType directory -Force "elasticsearch\\sources" > $null
-New-Item -ItemType directory -Force "logstash\\sources" > $null
-New-Item -ItemType directory -Force "kibana\\sources" > $null
-
-$openJdkZipPath = "sources\\" + $OpenJDKZipFile
-DownloadIfMissing $openJdkZipPath $OpenJDKUri
+$openJdkZipPath = $PSScriptRoot + "\\sources\\" + $OpenJDKZipFile
+Download-If-Missing $openJdkZipPath $OpenJDKUri
 if ((Test-Path -Path "sources\\java*" -Exclude *.zip) -eq $false) {
     Expand-Archive -Path $openJdkZipPath -DestinationPath "sources"
 }
+
 $jdkFolder = Get-JDK-App "sources"
 if($null -ne $jdkFolder) {
     Copy-App $jdkFolder "sources" "elasticsearch\\sources\\"
     Copy-App $jdkFolder "sources" "logstash\\sources\\"
 }
 
-$elasticSearchZipPath = "sources\\" + $ElasticSearchOssZipFile
-DownloadIfMissing $elasticSearchZipPath $ElasticSearchOssDownloadUri
-Copy-Item -Path $elasticSearchZipPath -Destination "elasticsearch\\sources\\${ElasticSearchOssZipFile}" -Force
+$elasticSearchZipPath = $PSScriptRoot + "\\sources\\" + $ElasticSearchOssZipFile
+Download-If-Missing $elasticSearchZipPath $ElasticSearchOssDownloadUri
+Copy-File-If-Missing $elasticSearchZipPath "elasticsearch\\sources\\${ElasticSearchOssZipFile}"
 
-$logstashZipPath = "sources\\" + $LogstashOssZipFile
-DownloadIfMissing $logstashZipPath $LogstashOssDownloadUr
-Copy-Item -Path $logstashZipPath -Destination "logstash\\sources\\${LogstashOssZipFile}" -Force
+$logstashZipPath = $PSScriptRoot + "\\sources\\" + $LogstashOssZipFile
+Download-If-Missing $logstashZipPath $LogstashOssDownloadUri
+Copy-File-If-Missing $logstashZipPath "logstash\\sources\\${LogstashOssZipFile}"
 
-$kibanaZipPath = "sources\\" + $KibanaOssZipfile
-DownloadIfMissing $kibanaZipPath $KibanaOssDownloadUri
-Copy-Item -Path $kibanaZipPath -Destination "kibana\\sources\\${KibanaOssZipfile}" -Force
+$kibanaZipPath = $PSScriptRoot + "\\sources\\" + $KibanaOssZipfile
+Download-If-Missing $kibanaZipPath $KibanaOssDownloadUri
+Copy-File-If-Missing $kibanaZipPath "kibana\\sources\\${KibanaOssZipfile}"
